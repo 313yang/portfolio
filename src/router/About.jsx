7e75f4a05d4df68331a888bcee5af723583092f2
yangbreoll <78190786+yangbreoll@users.noreply.github.com>
2021-06-30 01:35:13 +0900

    add github api

--- src/router/About.jsx
+++ src/router/About.jsx
@@ -1,6 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "../assets/About.css";
 
 function About() {
+  const [languages, setLanguages] = useState([]);
+  useEffect(() => {
+    const getLanguages = async () => {
+      const owner = "313yang";
+      const repo = "skill";
+      const { data } = await axios.get(
+        `https://api.github.com/repos/${owner}/${repo}/languages`,
+        {
+          headers: {
+            accept: "application/vnd.github.v3+json",
+          },
+        }
+      );
+      setLanguages(data);
+    };
+    getLanguages();
+  }, [languages]);
   const string =
     "안녕하세요. 프론트엔드 개발자를 꿈꾸는 양벼리입니다.\n Javascript를 위주로 다양한 토이 프로젝트를 만드는 것을 좋아합니다.\n 현실에 안주하지 않고 끊임없이 배우는 개발자로 남는 것이 목표입니다.";
   return (
@@ -11,6 +30,7 @@ function About() {
       </div>
       <div className="skills">
         <h1>Skills</h1>
+        <span></span>
       </div>
     </section>
   );
