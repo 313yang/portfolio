c64ef677ef1d926e1905c0c586841a3ce2f2b1e2
yangbreoll <78190786+yangbreoll@users.noreply.github.com>
2021-07-02 01:36:00 +0900

    add project-contents, css

---
+++ src/components/ProjectContent.js
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import "../assets/Project.css";
+function ProjectContent({ title, description, pageLink, githubLink }) {
+  return (
+    <>
+      <div className="project-section__content">
+        <div className="project-section__content__img">
+          <img src={`./img/${title}.PNG`} alt={title} title={title} />
+        </div>
+        <div className="project-section__content__btns">
+          <p>{description}</p>
+          <a href={pageLink} className="project-section__link">
+            <button className="project-section__linkBtn">GO TO PAGE</button>
+          </a>
+          <a href={githubLink} className="project-section__link">
+            <button className="project-section__linkBtn">GITHUB</button>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+export default ProjectContent;
+
+ProjectContent.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  pageLink: PropTypes.string.isRequired,
+  githubLink: PropTypes.string.isRequired,
+};
