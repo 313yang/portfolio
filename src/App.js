3aba639ced791f6f270b5b0c2ff748b204c5f40e
yangbreoll <78190786+yangbreoll@users.noreply.github.com>
2021-07-04 00:55:33 +0900

    add nav li scrollY event

--- src/App.js
+++ src/App.js
@@ -5,19 +5,45 @@ import Projects from "./router/Projects";
 import Contact from "./router/Contact";
 
 import "./assets/styles.css";
+import { useEffect } from "react";
 function App() {
   const handleScroll = (e) => {
     const {
       target: { id },
     } = e;
     if (id === "about") {
-      window.scrollTo({ top: 1000, behavior: "smooth" });
+      window.scrollTo({ top: 950, behavior: "smooth" });
     } else if (id === "projects") {
-      window.scrollTo({ top: 2000, behavior: "smooth" });
+      window.scrollTo({ top: 2100, behavior: "smooth" });
     } else if (id === "contact") {
       window.scrollTo({ top: 3000, behavior: "smooth" });
     }
   };
+  useEffect(() => {
+    const scrollEvent = () => {
+      const about = document.getElementById("about");
+      const project = document.getElementById("projects");
+      const contact = document.getElementById("contact");
+
+      if (window.scrollY <= 2000 && window.scrollY > 500) {
+        about.style.opacity = "1";
+        project.style.opacity = "0.6";
+        contact.style.opacity = "0.6";
+      } else if (window.scrollY <= 2900 && window.scrollY > 2000) {
+        project.style.opacity = "1";
+        about.style.opacity = "0.6";
+        contact.style.opacity = "0.6";
+      } else if (window.scrollY > 2900) {
+        contact.style.opacity = "1";
+        project.style.opacity = "0.6";
+        about.style.opacity = "0.6";
+      } else {
+        about.style.opacity = "0.6";
+      }
+    };
+    window.addEventListener("scroll", scrollEvent);
+    return () => window.removeEventListener("scroll", scrollEvent);
+  }, []);
   return (
     <>
       <HashRouter>
